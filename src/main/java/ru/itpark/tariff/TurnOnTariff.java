884b56bc4b330dabd911d626fa1c78e914e7ab71
Roman <groma_92@mail.ru>
2019-11-08 22:39:37 +0300

    fixes #6 close

--- src/main/java/ru/itpark/tariff/TurnOnTariff.java
+++ src/main/java/ru/itpark/tariff/TurnOnTariff.java
@@ -6,5 +6,6 @@ public class TurnOnTariff extends BasicTariff {
     public TurnOnTariff(long id, String name, int subscriptionFee, String internet, String feature, int period, int sms) {
         super(id, name, subscriptionFee, internet, feature, period);
         this.sms = sms;
+
     }
 }
